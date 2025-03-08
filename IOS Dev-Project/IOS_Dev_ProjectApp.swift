cbe6047f8fa451cad985432e61f569fd872fd397
Chanikya_877 <chanikya0527@gmail.com>
2025-03-07 20:00:36 -0500

    Added Firebase

--- IOS Dev-Project/IOS_Dev_ProjectApp.swift
+++ IOS Dev-Project/IOS_Dev_ProjectApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
 
+    return true
+  }
+}
 @main
 struct IOS_Dev_ProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            DashboardView() // This is now the entry point
+            NavigationView{DashboardView()}
+             // This is now the entry point
         }
     }
 }
